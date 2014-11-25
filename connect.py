03cb22731a330603ee1194d7cde6c0ed13f29e70
Thomas Levine <_@thomaslevine.com>
2014-11-25 02:08:52 +0000

    -

---
+++ connect.py
@@ -0,0 +1,25 @@
+import numpy
+
+def load(data):
+    '''
+    [[str]] -> [numpy array]
+
+    List of rows to list of numpy arrays,
+    one numpy array per column, in order
+    '''
+
+    for row in data:
+        try:
+            columns
+        except NameError:
+            columns = [[] * len(row)]
+
+        for i, cell in enumerate(row):
+            try:
+                floatcell = float(cell)
+            except TypeError:
+                pass
+            else:
+                columns[i].append(floatcell)
+
+    return map(numpy.array, columns)
