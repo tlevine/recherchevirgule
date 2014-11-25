314c42a6add2c3aa583b2bce72928c1f794c0599
Thomas Levine <_@thomaslevine.com>
2014-11-25 01:36:56 +0000

    rename

--- online-statistics.hs
+++ online-statistics.hs
@@ -10,3 +10,11 @@ instance Statistic Mean where
   initialize = Mean 0 0
   reduce (Mean sum n) x = Mean (sum + x) (n + 1)
   finalize (Mean sum n) = sum / ((fromIntegral n) :: Float)
+
+data Max = Max Float
+instance Statistic Max where
+  initialize = Max 0
+  reduce (Max x) y = Max $ max x y
+  finalize (Max x) = x
+
+data 
